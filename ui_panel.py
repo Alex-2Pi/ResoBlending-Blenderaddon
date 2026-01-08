4dfa393bfdfa8d7a0e9491427b1e7e8871c16f93
Alex_2Pi <35408350+Alex-2Pi@users.noreply.github.com>
2026-01-08 21:25:31 +0100

    Rename socket UI panel and update operators
    Updated panel class name and socket operator methods for clarity.

--- ui_panel.py
+++ ui_panel.py
@@ -1,9 +1,10 @@
+# ui_panel.py
 import bpy
 
 
-class VIEW3D_PT_socket_ui(bpy.types.Panel):
-    bl_label = "Resonite_Connection"
-    bl_idname = "VIEW3D_PT_socket_ui"
+class VIEW3D_PT_reso_connection(bpy.types.Panel):
+    bl_label = "Resonite Connection"
+    bl_idname = "VIEW3D_PT_reso_connection"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'ResoBlending'
@@ -12,13 +13,15 @@ class VIEW3D_PT_socket_ui(bpy.types.Panel):
         layout = self.layout
         scene = context.scene
 
-        layout.prop(scene, "socket_host")
-        layout.prop(scene, "socket_port")
+        col = layout.column()
+        col.enabled = not scene.socket_connected
+        col.prop(scene, "socket_host")
+        col.prop(scene, "socket_port")
 
         if scene.socket_connected:
-            layout.operator("my.socket_disconnect", icon='CANCEL')
+            layout.operator("reso.disconnect", icon='CANCEL')
         else:
-            layout.operator("my.socket_connect", icon='LINKED')
+            layout.operator("reso.connect", icon='LINKED')
 
         box = layout.box()
         icon = 'CHECKMARK' if scene.socket_connected else 'ERROR'
